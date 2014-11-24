4546289eac08708049c1db52c34f67b7e85c4262
ErichClark <erich.clark@gmail.com>
2014-11-24 16:44:04 -0500

    finished Ch3

---
+++ FlashyThingVs13/FlashyThingVs13/FlashyThing.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlashyThingVs13
+{
+    public partial class FlashyThing : Form
+    {
+        public FlashyThing()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            while (Visible)
+            {
+                for (int c = 0; c < 253 && Visible; c++)
+                {
+                    this.BackColor = Color.FromArgb(c, 255 - c, c);
+                    Application.DoEvents();
+                    System.Threading.Thread.Sleep(3);
+                }
+
+                for (int d = 253; d > 0 && Visible; d--)
+                {
+                    this.BackColor = Color.FromArgb(d, 255 - d, d);
+                    Application.DoEvents();
+                    System.Threading.Thread.Sleep(3);
+                }
+            }
+        }
+    }
+}
